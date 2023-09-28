2a411c675c3615e9a52f4f7855e9a1a706d6ad4a
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2023-09-28 10:21:12 +0300

    Initial commit

---
+++ Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            {/* Joika logo */}
+            <Text>Logo</Text>
+            {/* Navigation */}
+            <Text>Navigation 1</Text>
+            <Text>Navigation 2</Text>
+        </View>
+    );
+};
+
+export default Home;
