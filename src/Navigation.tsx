53f12446cc3fb8f603468f5edc450ce40f899a62
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2023-10-16 15:08:35 +0300

    Adding Vector Icons for navigation
    Had to remove and re-install node_modules to get Ionicons (react-native-vector-icons) working.
    Icons still not displaying properly.

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Ionicons from 'react-native-vector-icons/Ionicons'; // Icon library
+
 import Home from './Home';
 
 const Tab = createBottomTabNavigator();
@@ -9,7 +12,16 @@ const Navigation = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name='home' component={Home} />
+                <Tab.Screen
+                    name="home"
+                    component={Home}
+                    options={{
+                        tabBarLabel: 'Home',
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name='home-sharp' size={32} color='black' />
+                        ),
+                    }}
+                />
                 {/* More screens here */}
             </Tab.Navigator>
         </NavigationContainer>
