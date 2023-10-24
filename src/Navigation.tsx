605164c209fdfb349cbee76c8cbbd4b9cb32aca6
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2023-10-24 12:00:47 +0300

    Updating bottom navigation
    Added new icons for bottom navigation and placeholders for future components.

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,27 +1,52 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
 import Ionicons from 'react-native-vector-icons/Ionicons'; // Icon library
-
 import Home from './Home';
 
 const Tab = createBottomTabNavigator();
+
+// Placeholder for Search component
+const SearchPlaceholder = () => (
+    <></> // Empty fragment that doesn't render anything
+)
+
+// Placeholder for Map component
+const MapPlaceholder = () => (
+    <></> // Empty fragment that doesn't render anything
+)
 
 const Navigation = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator>
                 <Tab.Screen
+                    name="search"
+                    component={SearchPlaceholder}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="search" size={32} color='black' />
+                        ),
+                    }}
+                />
+                <Tab.Screen
                     name="home"
                     component={Home}
                     options={{
                         tabBarIcon: ({ color, size }) => (
-                            <Ionicons name='home-sharp' size={32} color='black' />
+                            <Ionicons name='home' size={32} color='black' />
                         ),
                     }}
                 />
-                {/* More screens here */}
+                <Tab.Screen
+                    name="map"
+                    component={MapPlaceholder}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name='compass' size={32} color='black' />
+                        ),
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
